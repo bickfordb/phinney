5f461516921cd691b8562b66d92850adc6984edc
Brandon Bickford <bickfordb@gmail.com>
2013-04-20 09:28:42 -0700

    add security and json

---
+++ request.go
@@ -0,0 +1,27 @@
+package phinney
+
+import (
+  "encoding/json"
+  "fmt"
+  "regexp")
+
+var JSONContentType *regexp.Regexp = regexp.MustCompile(`^.*application[/]json\s*(?:[;].+)$`)
+
+func (r *Request) JSONParams(record interface {}) (e error) {
+  contentType := r.Request.Header.Get("Content-Type")
+  if JSONContentType.MatchString(contentType) {
+    e = fmt.Errorf("expecting JSON Content-Type but got %q", contentType)
+    return
+  }
+  dec := json.NewDecoder(r.Request.Body)
+  e = dec.Decode(record)
+  return
+}
+
+func (r *Request) JSONResponse(record interface {}) (err error) {
+  r.Response.Header().Set("Content-Type", "application/json")
+  enc := json.NewEncoder(r.Response)
+  err = enc.Encode(record)
+  return
+}
+
---
+++ security_test.go
@@ -0,0 +1,17 @@
+package phinney
+
+import (
+  "testing"
+)
+
+func TestSecureString(t *testing.T) {
+  secret := []byte("hello")
+  xs := SecureString1("hi", secret)
+  result, _ := CheckString1(xs, secret)
+  expected := "hi"
+  if result != expected {
+    t.Errorf("expecting %q but got %q (%q)", expected, result, xs)
+  }
+}
+
+
